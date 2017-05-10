cf9a63030ba5b822336ef3ef8a0801791ea4042c
Dimitar Mitev <dnmitev@gmail.com>
2017-05-10 16:31:27 +0300

    clean umbraco install

--- HandsOnUmbraco/HandsOnUmbraco/Properties/AssemblyInfo.cs
+++ HandsOnUmbraco/HandsOnUmbraco/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e8ff5e7a-5ce3-49bd-8baf-2f3870c7f92c")]
+[assembly: Guid("7f814211-15a7-488a-8c79-afce2249fe5e")]
 
 // Version information for an assembly consists of the following four values:
 //
